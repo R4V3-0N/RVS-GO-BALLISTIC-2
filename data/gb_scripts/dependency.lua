80c2dccb4e626354e141963d59484ca8a6771a37
ChronoVortex <nick@hudsucker.com>
2023-04-06 22:40:44 -0700

    update version, add check for hs 1.3

--- data/gb_scripts/dependency.lua
+++ data/gb_scripts/dependency.lua
@@ -1,3 +1,6 @@
+if not Hyperspace.version or Hyperspace.version.major < 1 or Hyperspace.version.minor < 3 then
+    error("Incorrect Hyperspace version detected! Go Ballistic requires Hyperspace 1.3+")
+end
 if not mods or not mods.inferno then
-    error("Couldn't find Inferno Core! Make sure it's above mods which depend on it in the Slipstream load order", 2)
+    error("Couldn't find Inferno Core! Make sure it's above mods which depend on it in the Slipstream load order")
 end
